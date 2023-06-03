1f8822cb3ab5d6408c9d09072eb50e071ef62009
aimadnet <contact@aimadnet.com>
2023-06-03 18:49:19 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='sleeptimer',
-    version='1.0.0',
+    version='1.0.1',
     description='A sleep timer package with live timer and progress bar',
     author='Aimadnet',
     author_email='contact@aimadnet.com',
     packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires=[
-        'termios',
-    ],
+    python_requires='>=3.6'
 )
